f4c3c4f311e2dadd76613a563b30fc8dda8a8e60
paul-war <pablodanielguerra@gmail.com>
2023-04-17 08:17:18 +0000

    family api entrega

--- src/app.py
+++ src/app.py
@@ -26,17 +26,37 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_all_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
-
+@app.route('/member', methods=['POST'])
+def add_member():
+    request_body = request.json
+    member = {'id': request_body.get('id') or family._generateId(),
+              'first_name': request_body.get('first_name'),
+              'age': request_body.get('age'),
+              'lucky_numbers': request_body.get('lucky_numbers')}
+    if not all(member.values()):
+        return jsonify({"message": "Not Found"}), 400
+    response_body = jackson_family.add_member(member)
     return jsonify(response_body), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member_by_id(id):
+    member = jackson_family.get_member(id)
+    if member:
+        return jsonify(member[0]), 200
+    else:
+        return jsonify({"message": "Not Found"}), 400
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    done = jackson_family.delete_member(id)
+    if done:
+        return jsonify({"done": True}), 200
+    else:
+        return jsonify({"message": "Not Found"}), 400
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,23 +13,45 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{"id": self._generateId(),
+                          "first_name": "John",
+                          "last_name": last_name,
+                          "age": 33,
+                          "lucky_numbers": [7, 13, 22]},
+                         {"id": self._generateId(),
+                          "first_name": "Jane",
+                          "last_name": last_name,
+                          "age": 35,
+                          "lucky_numbers": [10, 14, 3]},
+                         {"id": self._generateId(),
+                          "first_name": "Jimmy",
+                          "last_name": last_name,
+                          "age": 5,
+                          "lucky_numbers": [1]}]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        member["last_name"] = self.last_name
+        self._members.append(member)
+        return self._members
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        members = []
+        for member in self._members:
+            if member["id"] != id:
+                members.append(member)
+        self._members = members
+        return self._members
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        members = []
+        for member in self._members:
+            if member["id"] == id:
+                members.append(member)
+        return members
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
